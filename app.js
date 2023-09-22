063d7695bcd3467861cfe72085cee87495f30429
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-09-22 15:48:39 +0200

    refactor : create class Sketch

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-// init
 export default class Sketch {
   constructor(options) {
-    this.render();
+    this.container = options.domElement;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(this.width, this.height);
+
+    this.container.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
     this.time = 0;
+    this.addObjects();
+    this.render();
   }
+
+  addObjects() {
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
   render() {
     this.time += 0.05;
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
+
+    this.renderer.render(this.scene, this.camera);
     console.log(this.time);
     requestAnimationFrame(this.render.bind(this));
   }
 }
 
-new Sketch();
-
-// const camera = new THREE.PerspectiveCamera(
-//   70,
-//   window.innerWidth / window.innerHeight,
-//   0.01,
-//   10
-// );
-// camera.position.z = 1;
-
-// const scene = new THREE.Scene();
-
-// const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-// const material = new THREE.MeshNormalMaterial();
-
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
-
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setAnimationLoop(animation);
-// let container = document.getElementById("container");
-// container.appendChild(renderer.domElement);
-
-// // animation
-
-// function animation(time) {
-//   mesh.rotation.x = time / 2000;
-//   mesh.rotation.y = time / 1000;
-
-//   renderer.render(scene, camera);
-// }
+new Sketch({
+  domElement: document.getElementById("container"),
+});
