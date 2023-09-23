70d711f6726e1553b171da8e5519d239bbfaaab5
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-09-23 21:18:43 +0200

    feat : water animate texture

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
+import testTexture from "./assets/water.jpg";
 
 export default class Sketch {
   constructor(options) {
@@ -42,9 +43,10 @@ export default class Sketch {
   addObjects() {
     this.geometry = new THREE.PlaneGeometry(0.5, 0.5, 100, 100);
     this.material = new THREE.ShaderMaterial({
-      wireframe: true,
+      // wireframe: true,
       uniforms: {
         time: { value: 1.0 },
+        uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         resolution: { value: new THREE.Vector2() },
       },
       vertexShader: vertex,
